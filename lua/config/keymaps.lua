16980720ddfe7d58368dcf3e8cbf37cbcf5a1042
da-br <brandendj@gmail.com>
2024-01-04 18:45:33 +0100

    new conf

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,13 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+local map = vim.keymap.set
+-- Copy to clipboard
+map({ "n", "v" }, "<leader>y", "+y", { remap = false })
+map("n", "<leader>Y", "+yg_", { remap = false })
+map("n", "<leader>yy", "+yy", { remap = false })
+
+-- Paste from clipboard
+map({ "n", "v" }, "<leader>p", "+p", { remap = false })
+map({ "n", "v" }, "<leader>P", "+P", { remap = false })
