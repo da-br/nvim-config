7a0ea2e3333a4143df2d94b0877e81b3102f719f
Daniel Brandenburg <dabr@dhigroup.com>
2024-01-05 14:19:24 +0100

    init local

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -6,7 +6,6 @@ return {
         "bash",
         "html",
         "javascript",
-        "json",
         "lua",
         "markdown",
         "markdown_inline",
@@ -17,21 +16,10 @@ return {
         "typescript",
         "vim",
         "yaml",
+        "json",
+        "json5",
+        "jsonc",
       },
     },
-  },
-
-  -- since `vim.tbl_deep_extend`, can only merge tables and not lists, the code above
-  -- would overwrite `ensure_installed` with the new value.
-  -- If you'd rather extend the default config, use the code below instead:
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = function(_, opts)
-      -- add tsx and treesitter
-      vim.list_extend(opts.ensure_installed, {
-        "tsx",
-        "typescript",
-      })
-    end,
   },
 }
