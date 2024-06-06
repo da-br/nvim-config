77dd7911266677150b198e95178e5ca54eb6f815
da-br <brandendj@gmail.com>
2024-06-06 21:35:07 +0200

    debugging

---
+++ lua/plugins/dap.lua
@@ -0,0 +1,47 @@
+return {
+	{
+		"mfussenegger/nvim-dap",
+		dependencies = {
+			"leoluz/nvim-dap-go",
+			"rcarriga/nvim-dap-ui",
+			"nvim-neotest/nvim-nio",
+			"williamboman/mason.nvim",
+			"theHamsta/nvim-dap-virtual-text",
+		},
+		config = function()
+			local dap = require("dap")
+			local ui = require("dapui")
+
+			require("dapui").setup()
+			require("dap-go").setup()
+			require("nvim-dap-virtual-text").setup()
+
+			-- Eval var under cursor
+			vim.keymap.set("n", "<space>?", function()
+				require("dapui").eval(nil, { enter = true })
+			end)
+
+			vim.keymap.set("n", "<F5>", dap.continue)
+			vim.keymap.set("n", "<F11>", dap.step_into)
+			vim.keymap.set("n", "<F10>", dap.step_over)
+			vim.keymap.set("n", "<s-F11>", dap.step_out)
+			vim.keymap.set("n", "<F4>", dap.step_back)
+			vim.keymap.set("n", "<C-F10>", dap.run_to_cursor)
+			vim.keymap.set("n", "<F9>", dap.toggle_breakpoint)
+			vim.keymap.set("n", "<C-S-F5>", dap.restart)
+
+			dap.listeners.before.attach.dapui_config = function()
+				ui.open()
+			end
+			dap.listeners.before.launch.dapui_config = function()
+				ui.open()
+			end
+			dap.listeners.before.event_terminated.dapui_config = function()
+				ui.close()
+			end
+			dap.listeners.before.event_exited.dapui_config = function()
+				ui.close()
+			end
+		end,
+	},
+}
